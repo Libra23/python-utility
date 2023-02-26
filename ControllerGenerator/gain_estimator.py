7b78f828170769f9682a8e537b470647af3bb1aa
Libra23 <kaya-libra-23@hotmail.co.jp>
2023-02-27 07:31:19 +0900

    add filter

--- ControllerGenerator/gain_estimator.py
+++ ControllerGenerator/gain_estimator.py
@@ -29,14 +29,16 @@ class gain_estimator:
             self.ctrl_func = self.pdf
         self.dt = dt
 
-    def eval_func(self, rho, u0, y0, Td):
+    def eval_func(self, rho, u0, y0, Td, W):
         invC = self.ctrl_func(rho) ** -1
-        e_pseudo, _, _ = control.lsim(invC, u0.flatten())
-        r_pseudo = e_pseudo + y0
+        u_filt, _, _ = control.lsim(W, u0.flatten())
+        y_filt, _, _ = control.lsim(W, y0.flatten())
+        e_pseudo, _, _ = control.lsim(invC, u_filt.flatten())
+        r_pseudo = e_pseudo + y_filt
         y_pseudo, _, _ = control.lsim(Td, r_pseudo.flatten())
-        return np.linalg.norm(y0 - y_pseudo)**2
+        return np.linalg.norm(y_filt - y_pseudo)**2
 
-    def estimate(self, rho0, input, output, Td):
+    def estimate(self, rho0, input, output, Td, W):
         from scipy.optimize import minimize
         from scipy.optimize import LinearConstraint, BFGS
 
@@ -50,7 +52,7 @@ class gain_estimator:
 
         ret = minimize( fun=self.eval_func,
                         x0 = rho0,
-                        args = (input, output, Td),
+                        args = (input, output, Td, W),
                         method="trust-constr", 
                         jac="2-point",
                         hess=BFGS(),
@@ -65,15 +67,22 @@ class gain_estimator:
 if __name__=="__main__":
     from model_creator import *
     mc = model_creator()
-    # mc.create_model_1st(1.02, 0.74)
-    mc.create_model_2nd(1.02, 20, 0.8)
+    mc.create_model_1st(1.02, 0.74)
+    #mc.create_model_2nd(1.02, 20, 0.8)
     u = np.ones(1000)
     dt = 0.001
     y, t = mc.generate_dataset(u, dt, False, False)
 
     ge = gain_estimator()
-    ge.select_model('pdf', dt)
+    ge.select_model('pid', dt)
     rho0 = np.array([10.0, 0.0, 0.0])
     tau = 0.02
     Td = control.c2d(control.tf([1], [tau, 1]), dt)
-    ge.estimate(rho0, u, y, Td)
+    fc = 5.0
+    W = control.c2d(control.tf([1], [1 / (2 * np.pi * fc), 1]), dt)
+    rho = ge.estimate(rho0, u, y, Td, W)
+    print('rho = ',rho)
+    C = ge.ctrl_func(rho)
+    plt.figure()
+    control.bode(C)
+    plt.show()
